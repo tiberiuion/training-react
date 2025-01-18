106ee8ad4083aae633bcc9a7b4ece56fec4a3de4
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-01-18 13:33:38 +0000

    Pass data through props.

--- react-dev/tutorial-tic-tac-toe/src/App.js
+++ react-dev/tutorial-tic-tac-toe/src/App.js
@@ -1,26 +1,27 @@
-function Square() {
-  return  <button className="square">1</button>;
+function Square({value}) {
+  // Curly braces {} are used to escape from jsx into js and execute js code. 
+  return  <button className="square">{value}</button>;
 };
 export default function Board() {
   return (
   <>
   <div className="board-row">
     {/* Custom components start with a capital letter */}
-   <Square />
-   <Square />
-   <Square />
+   <Square value="1" />
+   <Square value="2" />
+   <Square value="3" />
   </div>
 
 <div className="board-row">
-   <Square />
-   <Square />
-   <Square />
+   <Square value="4" />
+   <Square value="5" />
+   <Square value="6" />
 </div>
 
 <div className="board-row">
-   <Square />
-   <Square />
-   <Square />
+   <Square value="7" />
+   <Square value="8" />
+   <Square value="9" />
 </div>
 
   </>
