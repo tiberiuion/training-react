4dde135709c7d24638bb45d561033411c409c4bf
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-01-19 09:29:29 +0000

    Lift state to the board component.

--- react-dev/tutorial-tic-tac-toe/readme.md
+++ react-dev/tutorial-tic-tac-toe/readme.md
@@ -14,3 +14,8 @@ Making components interactive
 
 Adding state to a component
  
+ Lifting state up
+
+ "To collect data from multiple children, or to have two child components communicate with each other, declare the shared state in their parent component instead. The parent component can pass that state back down to the children via props. This keeps the child components in sync with each other and with their parent."
+ 
+ [React.dev](https://react.dev/learn/tutorial-tic-tac-toe#completing-the-game)
--- react-dev/tutorial-tic-tac-toe/src/App.js
+++ react-dev/tutorial-tic-tac-toe/src/App.js
@@ -4,45 +4,31 @@
 // Import sate.
 import { useState } from 'react';
 
-function Square() {
-  // Add state to the component.
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    // Update value to X when component is clicked.
-    setValue('X');
-  };
-
+function Square({value}) {
   // Curly braces {} are used to escape from jsx into js and execute js code. 
-  return  (
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-        {value}
-    </button>
-  );
+  return  <button className="square">{value}</button>;
 };
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
   <>
   <div className="board-row">
     {/* Custom components start with a capital letter */}
-   <Square  />
-   <Square  />
-   <Square  />
+   <Square value={squares[0]}  />
+   <Square value={squares[1]}  />
+   <Square value={squares[2]}  />
   </div>
 
 <div className="board-row">
-   <Square  />
-   <Square  />
-   <Square  />
+   <Square value={squares[3]}  />
+   <Square value={squares[4]}  />
+   <Square value={squares[5]}  />
 </div>
 
 <div className="board-row">
-   <Square  />
-   <Square  />
-   <Square  />
+   <Square value={squares[6]}  />
+   <Square value={squares[7]}  />
+   <Square value={squares[8]}  />
 </div>
 
   </>
