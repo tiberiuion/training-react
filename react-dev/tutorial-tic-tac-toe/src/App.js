4896f23a9c47d8a9d1dd675d4545f7688a0bb431
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-02-05 23:07:44 +0000

    Lift state to the Game component.

--- react-dev/tutorial-tic-tac-toe/src/App.js
+++ react-dev/tutorial-tic-tac-toe/src/App.js
@@ -8,9 +8,7 @@ function Square({ value, onSquareClick }) {
     // Curly braces {} are used to escape from jsx into js and execute js code. 
     return <button className="square" onClick={onSquareClick}>{value}</button>;
 };
-export default function Board() {
-    const[ xIsNext, setXIsNext ] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
     const winner = calculateWinner(squares);
     let status;
 
@@ -33,8 +31,7 @@ export default function Board() {
              nextSquares[i] = "O";
         };
 
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
     return (
         <>
@@ -59,6 +56,28 @@ export default function Board() {
             </div>
 
         </>
+    );
+}
+
+export default function Game() {
+    const[ xIsNext, setXIsNext ] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext)
+    };
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}  />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
     );
 }
 
