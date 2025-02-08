4340e13cee8b478d9a5f706edcd7d614c4aaaec7
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-02-08 13:10:30 +0000

    Add history functionality

--- react-dev/tutorial-tic-tac-toe/src/App.js
+++ react-dev/tutorial-tic-tac-toe/src/App.js
@@ -61,7 +61,7 @@ function Board({xIsNext, squares, onPlay}) {
 
 export default function Game() {
     const[ xIsNext, setXIsNext ] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [history, setHistory] = useState([Array(9).fill(null)]);
     const currentSquares = history[history.length - 1];
 
     function handlePlay(nextSquares) {
@@ -69,13 +69,32 @@ export default function Game() {
         setXIsNext(!xIsNext)
     };
 
+    function jumpTo(nextMove) {
+
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = "Go to move #" + move;
+        } else {
+            description = "Go to game start";
+        }
+
+        return (
+            <li>
+                <button onClick={()=> jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}  />
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
